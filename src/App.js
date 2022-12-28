777ebbd3b856bbb46075ca74af9202127b05e45e
Sandip Nepali <sandeep47nepali@gmail.com>
2022-12-28 12:18:47 +0545

    �� calculator lite using react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./assets/mero.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="main">
+        <input type="text" class="result" value="0" disabled/>
+        <div class="calculator-button">
+        <button type="button" class="operator1" value="+">+</button>
+        <button type="button" class="operator2" value="-">-</button>
+        <button type="button" class="operator3" value="*">&times;</button>
+        <button type="button" class="operator4" value="/">&divide;</button>
+
+        <button type="button" value="7">7</button>
+        <button type="button" value="8">8</button>
+        <button type="button" value="9">9</button>
+
+        <button type="button" class="button1" value="1">1</button>
+        <button type="button" class="button2" value="2">2</button>
+        <button type="button" class="button3" value="3">3</button>
+
+        <button type="button" class="button4" value="4">4</button>
+        <button type="button" class="button5" value="5">5</button>
+        <button type="button" class="button6" value="6">6</button>
+
+
+        <button type="button0" value="0">0</button>
+        <button type="button" class="decimal" value=".">.</button>
+        <button type="button" class="all-clear" value="all-clear">C</button>
+
+        <button type="button" class="equal-sign" value="=">=</button>
+        </div>
+      </div>
+  </div>
   );
 }
 
